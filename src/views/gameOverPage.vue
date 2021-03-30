bbe832681fd90c469d778bc17d2a45d171c5d928
Kayle <kwidmyer99@gmail.com>
2021-03-30 12:06:22 -0700

    Added game over page template

--- src/views/gameOverPage.vue
+++ src/views/gameOverPage.vue
@@ -3,7 +3,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Victory Page</ion-title>
+        <ion-title>Game Over Page</ion-title>
       </ion-toolbar>
     </ion-header>
 
@@ -15,7 +15,7 @@
       </ion-header>
 
       <div id="container">
-        <h1>Victory!</h1>
+        <h1>Game Over!</h1>
         <div>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
@@ -39,7 +39,7 @@ import {
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "victoryPage",
+  name: "gameOverPage",
   components: {
     IonContent,
     IonHeader,
