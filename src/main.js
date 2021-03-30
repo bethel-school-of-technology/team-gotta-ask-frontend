8edb4a734ba20d2a5ff897140a5b0d5532dde691
Connor Gorr <xcongiex@gmail.com>
2021-03-30 10:59:06 -0700

    Added code to main.js that fixes the ionIcons bug, icons should now be usable anywhere using the ion-icons tag.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
+import { addIcons } from 'ionicons';
+import * as allIcons from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -22,6 +24,22 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+const currentIcons = Object.keys(allIcons).map(i => {
+  const key = i.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`)
+  if(typeof allIcons[i] === 'string') {
+    return {
+      [key]: allIcons[i],
+    }
+  }
+  return {
+    ['ios-' + key]: allIcons[i].ios,
+    ['md-' + key]: allIcons[i].md,
+  };
+});
+
+const iconsObject = Object.assign({}, ...currentIcons);
+addIcons(iconsObject);
 
 const app = createApp(App)
   .use(IonicVue)
