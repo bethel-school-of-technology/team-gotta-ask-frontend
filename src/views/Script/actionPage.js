e73cd0b0dac376d546b8a20a83bafbf6e45f3971
Graham Naquin <graham@naquin.live>
2021-03-31 17:43:57 -0500

    split JS and CSS from action page, having issues. need to compare to main

---
+++ src/views/Script/actionPage.js
@@ -0,0 +1,21 @@
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonIcon
+  } from "@ionic/vue";
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "actionPage",
+    components: {
+      IonContent,
+      IonHeader,
+      IonPage,
+      IonTitle,
+      IonToolbar,
+      IonIcon
+    },
+  });
