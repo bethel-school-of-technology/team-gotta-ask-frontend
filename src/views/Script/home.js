3f9b560f78472d1702a74ba6dc030da0c2967ae3
Graham Naquin <graham@naquin.live>
2021-03-31 17:20:26 -0500

    I separated CSS and JS from the .vue for Home and into folders. Also linked them to home.vue

---
+++ src/views/Script/home.js
@@ -0,0 +1,19 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+  },
+});
