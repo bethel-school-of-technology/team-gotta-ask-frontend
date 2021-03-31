e1dfc611806ab1a5ef40fe8f4b88ba92ee4228e5
Graham Naquin <graham@naquin.live>
2021-03-31 18:41:41 -0500

    separated JS and CSS for long text page

---
+++ src/views/Script/longTextPage.js
@@ -0,0 +1,19 @@
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+  } from "@ionic/vue";
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "longTextPage",
+    components: {
+      IonContent,
+      IonHeader,
+      IonPage,
+      IonTitle,
+      IonToolbar,
+    },
+  });
