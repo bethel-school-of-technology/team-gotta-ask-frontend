4c3ef11409c464ded9c3eeca503f9282be5df1d3
Kayle <kwidmyer99@gmail.com>
2021-04-07 12:41:28 -0700

    connected front and back via get

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,4 @@
+//This exports the base url to prevent redundancy
 export const server = {
     baseURL: 'http://localhost:3000'
 }
--- src/views/Script/actionPage.js
+++ src/views/Script/actionPage.js
@@ -19,3 +19,17 @@ import {
       IonIcon
     },
   });
+
+  import { server } from '../../helper.js';
+  import axios from 'axios';
+
+  export function fetchPlayer() {
+    axios
+      .get(`${server.baseURL}/player/606d03b156487248908b74b2`) //hardcoded for time being
+      .then(data => (this.player = data.data));
+  }
+  export function fetchEnemy() {
+    axios
+      .get(`${server.baseURL}/enemy/Goblin`) //hardcoded "goblin" for time being
+      .then(data => (this.enemy = data.data));
+  }
