246f9c961a8e0f663422edfa90e302d758c2770d
Graham Naquin <graham@naquin.live>
2021-04-12 07:38:12 -0500

    axios testing day 2

--- src/views/Script/characterCreation.js
+++ src/views/Script/characterCreation.js
@@ -5,7 +5,8 @@ import {
     IonTitle,
     IonToolbar,
   } from "@ionic/vue";
-  import { defineComponent } from "vue";
+import { defineComponent } from "vue";
+import axios from "axios";
   
   export default defineComponent({
     name: "characterCreation",
@@ -18,6 +19,26 @@ import {
     },
   });
 
+  export const postPlayer = () => {
+    var name = document.getElementById("customName");
+    var hp = document.getElementById("hpInput");
+    var attack = document.getElementById("attackInput");
+    axios
+      .post (
+        ' /*url*/ ',
+        {
+          name: name,
+          hp: hp,
+          attack: attack
+        })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+  }
+
   export function checkNumbers(){
     var numbersInput = parseInt(document.getElementById("hpInput").value) + parseInt(document.getElementById("attackInput").value);
     console.log("does my code know how to add? : " + numbersInput);
@@ -26,6 +47,7 @@ import {
       alert("more than 10 points, please adjust values and try again")
     }
     else {
+      postPlayer();
       window.location.href = '/longTextPage'
     }
   }
