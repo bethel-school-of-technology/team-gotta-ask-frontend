4e352fe1fb3711fdff5104f8edd1766385fa3021
Kayle <kwidmyer99@gmail.com>
2021-04-14 11:03:56 -0700

    pushing to main

--- src/views/Script/characterCreation.js
+++ src/views/Script/characterCreation.js
@@ -25,27 +25,27 @@ import axios from "axios";
     return rando;
   }
 
-  export const postPlayer = () => {
-    var name = document.getElementById('customName').value;
-    var hp = 3 * (document.getElementById('hpInput').value) + 20;
-    var attack = 2 * (document.getElementById('attackInput').value) + 5;
+  // export const postPlayer = () => {
+  //   var name = document.getElementById('customName').value;
+  //   var hp = 3 * (document.getElementById('hpInput').value) + 20;
+  //   var attack = 2 * (document.getElementById('attackInput').value) + 5;
 
-    axios
-      .post (
-        `${server.baseURL}/player/create`,
-        {
-          name: name,
-          hp: hp,
-          attack: attack
-        })
-        .then(response => {
-          console.log(response.data.player._id);
-          localStorage.setItem('playerId', response.data.player._id);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-  }
+  //   axios
+  //     .post (
+  //       `${server.baseURL}/player/create`,
+  //       {
+  //         name: name,
+  //         hp: hp,
+  //         attack: attack
+  //       })
+  //       .then(response => {
+  //         console.log(response.data.player._id);
+  //         localStorage.setItem('playerId', response.data.player._id);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       });
+  // }
 
   export async function checkNumbers(){
     var numbersInput = parseInt(document.getElementById("hpInput").value) + parseInt(document.getElementById("attackInput").value);
