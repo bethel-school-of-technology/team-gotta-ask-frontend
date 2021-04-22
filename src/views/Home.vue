eb5f4287da84325681b0679a22a67a156593ce0e
Graham Naquin <graham@naquin.live>
2021-04-22 14:53:57 -0500

    house cleaning

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,18 @@
 <template>
   <ion-page>
-    <!-- <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Nimrod's Ascent</ion-title>
-      </ion-toolbar>
-    </ion-header> -->
 
     <ion-content>
-      <!-- <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Nimrod's Ascent</ion-title>
-        </ion-toolbar>
-      </ion-header> --> 
 
       <div id="container">
         <div id="purpleBox" class="nes-container is-rounded">
-          <h1 id="titleText">Nimrod's Ascent</h1>
+          <h1 id="titleText" >Nimrod's Ascent</h1>
         </div>
         <div id="startButton">
           <button class="nes-btn is-primary" @click="makeLocal()">New Game</button>
           <br>
           <button id='resume' v-show='ok' class="nes-btn is-success" @click="resume()">Resume</button>
         </div>
-        <!-- <div id="quitButton"> -->
-          <!--these buttons will most likely have to call script that initiates a request to the backend as well as routes to the correct page-->
-          <!-- <ion-button color="danger">Quit (Doesn't function)</ion-button>
-        </div> -->
+
       </div>
     </ion-content>
   </ion-page>
